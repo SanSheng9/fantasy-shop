8edd7aa2b9b55def0146319d21057d53c2ff936b
SanSheng9 <sansheng9@yadnex.ru>
2022-07-09 14:05:41 +1000

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,72 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view />
+  <div class="background">
+    <div class="shop">
+      <landscape-banner></landscape-banner>
+      <component :is="layout">
+        <router-view />
+      </component>
+    </div>
+  </div>
 </template>
 
+<script>
+import "ant-design-vue/dist/antd.css";
+import LandscapeBanner from "@/components/LandscapeBanner";
+
+// eslint-disable-next-line vue/no-export-in-script-setup
+export default {
+  components: { LandscapeBanner },
+  computed: {
+    layout() {
+      return this.$route.meta.layout;
+    },
+  },
+};
+</script>
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+.background {
+  background: rgb(45, 37, 58);
+  background: -moz-linear-gradient(
+    0deg,
+    rgba(45, 37, 58, 1) 49%,
+    rgba(8, 182, 222, 1) 64%,
+    rgba(55, 33, 131, 1) 100%
+  );
+  background: -webkit-linear-gradient(
+    0deg,
+    rgba(45, 37, 58, 1) 49%,
+    rgba(8, 182, 222, 1) 64%,
+    rgba(55, 33, 131, 1) 100%
+  );
+  background: linear-gradient(
+    0deg,
+    rgba(45, 37, 58, 1) 49%,
+    rgba(8, 182, 222, 1) 64%,
+    rgba(55, 33, 131, 1) 100%
+  );
+  filter: progid:DXImageTransform.Microsoft.gradient(startColorstr="#2d253a",endColorstr="#372183",GradientType=1);
+  transition: background 0.5s ease;
 }
-
-nav {
-  padding: 30px;
+.shop {
+  max-width: 100vw;
+  min-height: 200vh;
+  background-repeat: no-repeat;
+  background-size: 100%;
+  transition: background 0.5s ease;
+  overflow-x: hidden;
 }
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
+/*.change-theme {*/
+/*  position: fixed;*/
+/*  bottom: 2vh;*/
+/*  right: 2%;*/
+/*}*/
+a:active, /* активная/посещенная ссылка */
+a:hover,  /* при наведении */
+a {
+  text-decoration: none;
+  color: #666 !important;
 }
-
-nav a.router-link-exact-active {
-  color: #42b983;
+input:focus {
+  outline: none;
 }
 </style>
---
+++ src/components/LandscapeBanner.vue
@@ -0,0 +1,101 @@
+<template>
+  <section>
+    <img src="@/assets/2.png" id="back" alt="" />
+    <img src="@/assets/1.png" id="front" alt="" />
+    <div id="block" class="block">
+      <div id="text">Fantasy shop!</div>
+      <button id="button">Click me!</button>
+    </div>
+    <img src="@/assets/sun.png" id="sun" alt="sun" />
+  </section>
+</template>
+
+<script>
+import "ant-design-vue/dist/antd.css";
+
+export default {
+  name: "LandscapeBanner",
+  created() {
+    window.addEventListener("scroll", this.handleScroll);
+  },
+  unmounted() {
+    window.removeEventListener("scroll", this.handleScroll);
+  },
+  methods: {
+    handleScroll() {
+      let sky = document.getElementById("sun");
+      let front = document.getElementById("front");
+      let back = document.getElementById("back");
+      let block = document.getElementById("block");
+
+      let value = window.scrollY;
+      sky.style.left = value * 0.2 + "px";
+      back.style.top = value * 0.2 + "px";
+      front.style.top = value * 0.3 + "px";
+      block.style.marginTop = value * 1.1 + "px";
+      if (value >= 780) {
+        block.style.zIndex = 9;
+      } else {
+        block.style.zIndex = 11;
+      }
+    },
+  },
+};
+</script>
+
+<style scoped>
+section {
+  position: relative;
+  width: 100%;
+  height: 100vh;
+  padding: 100px;
+}
+section img {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+}
+.block {
+  position: absolute;
+  left: 36vw;
+  z-index: 11;
+  display: flex;
+  flex-direction: column;
+  align-content: space-between;
+  min-height: 400px;
+  align-items: center;
+  justify-content: space-between;
+}
+#text {
+  color: #eeeeee;
+  white-space: nowrap;
+  font-size: 6em;
+  font-weight: 800;
+  box-shadow: white;
+  text-transform: uppercase;
+}
+#button {
+  font-size: 3em;
+  padding: 8px 30px;
+  font-weight: 800;
+  color: rgb(45, 37, 58);
+  background-color: #eeeeeeff;
+  border: none;
+  border-radius: 40px;
+  text-decoration: none;
+  cursor: pointer;
+}
+#front {
+  z-index: 10;
+  margin-top: 285px;
+}
+#back {
+  z-index: 6;
+}
+section #sun {
+  z-index: 1;
+}
+</style>
